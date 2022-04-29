52d9b64c881bb422862e74336d72a12922bdc544
owencalvin <owen.gombas@gmail.com>
2022-04-29 23:22:15 +0200

    docker(front & back)

--- production/memento-back.sql
+++ production/memento-back.sql
@@ -1,10 +1,8 @@
 -- MySQL dump 10.13  Distrib 8.0.28, for macos11 (x86_64)
 --
--- Host: 127.0.0.1    Database: memento
+-- Host: 127.0.0.1    Database: memento_back
 -- ------------------------------------------------------
--- Server version	8.0.28
-
-USE memento_back;
+-- Server version	8.0.29
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -25,13 +23,13 @@ DROP TABLE IF EXISTS `languages`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `languages` (
-                             `id` int NOT NULL AUTO_INCREMENT,
-                             `lang` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
-                             `abbr` varchar(2) COLLATE utf8mb4_bin DEFAULT NULL,
-                             `creation_date` datetime DEFAULT NULL,
-                             `updated_date` datetime DEFAULT NULL,
-                             PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+  `id` int NOT NULL AUTO_INCREMENT,
+  `lang` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `abbr` varchar(2) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `creation_date` datetime DEFAULT NULL,
+  `updated_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,6 +38,7 @@ CREATE TABLE `languages` (
 
 LOCK TABLES `languages` WRITE;
 /*!40000 ALTER TABLE `languages` DISABLE KEYS */;
+INSERT INTO `languages` VALUES (1,'Français','fr',NULL,NULL),(2,'English','en',NULL,NULL),(3,'German',NULL,NULL,NULL);
 /*!40000 ALTER TABLE `languages` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -51,21 +50,21 @@ DROP TABLE IF EXISTS `lists`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `lists` (
-                         `id` int NOT NULL AUTO_INCREMENT,
-                         `name` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
-                         `words` json DEFAULT NULL,
-                         `creator` int DEFAULT NULL,
-                         `lang_source` int DEFAULT NULL,
-                         `lang_def` int DEFAULT NULL,
-                         `creation_date` datetime DEFAULT NULL,
-                         `updated_date` datetime DEFAULT NULL,
-                         PRIMARY KEY (`id`),
-                         KEY `fk_users_idx` (`creator`),
-                         KEY `fk_lang_source_idx` (`lang_source`),
-                         KEY `fk_lang_def_idx` (`lang_def`),
-                         CONSTRAINT `fk_lang_def` FOREIGN KEY (`lang_def`) REFERENCES `languages` (`id`),
-                         CONSTRAINT `fk_lang_source` FOREIGN KEY (`lang_source`) REFERENCES `languages` (`id`),
-                         CONSTRAINT `fk_users` FOREIGN KEY (`creator`) REFERENCES `users` (`id`)
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `words` json DEFAULT NULL,
+  `creator` int DEFAULT NULL,
+  `lang_source` int DEFAULT NULL,
+  `lang_def` int DEFAULT NULL,
+  `creation_date` datetime DEFAULT NULL,
+  `updated_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_users_idx` (`creator`),
+  KEY `fk_lang_source_idx` (`lang_source`),
+  KEY `fk_lang_def_idx` (`lang_def`),
+  CONSTRAINT `fk_lang_def` FOREIGN KEY (`lang_def`) REFERENCES `languages` (`id`),
+  CONSTRAINT `fk_lang_source` FOREIGN KEY (`lang_source`) REFERENCES `languages` (`id`),
+  CONSTRAINT `fk_users` FOREIGN KEY (`creator`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -86,15 +85,15 @@ DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `users` (
-                         `id` int NOT NULL AUTO_INCREMENT,
-                         `login` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
-                         `email` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
-                         `password` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
-                         `creation_date` datetime DEFAULT NULL,
-                         `updated_date` datetime DEFAULT NULL,
-                         PRIMARY KEY (`id`),
-                         UNIQUE KEY `email_unique` (`email`),
-                         UNIQUE KEY `login_unique` (`login`)
+  `id` int NOT NULL AUTO_INCREMENT,
+  `login` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `email` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `creation_date` datetime DEFAULT NULL,
+  `updated_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email_unique` (`email`),
+  UNIQUE KEY `login_unique` (`login`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -116,4 +115,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-04-06  9:17:09
+-- Dump completed on 2022-04-29 23:21:28
