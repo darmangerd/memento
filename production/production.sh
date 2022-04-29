89f66087bf3f8c5b744ef18047488d3ebc255f63
owencalvin <owen.gombas@gmail.com>
2022-04-29 20:50:44 +0200

    docker(front & back)

--- production/production.sh
+++ production/production.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+docker image rm -f production_nginx
+docker image rm -f production_php
 docker-compose down -v
-docker-compose rm -sf 
+docker-compose rm -sf
 docker-compose --env-file .env up
