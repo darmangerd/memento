40fbfe076fbdf5f75681c42ed7b7fdbb63b947b7
owencalvin <owen.gombas@gmail.com>
2022-04-30 10:12:08 +0200

    fix(ErrorBuilder)

--- production/production.sh
+++ production/production.sh
@@ -4,4 +4,4 @@ docker image rm -f production_nginx
 docker image rm -f production_php
 docker-compose down -v
 docker-compose rm -sf
-docker-compose --env-file ../memento-back/.env up
+docker-compose --env-file ../memento-back/.env up -d
